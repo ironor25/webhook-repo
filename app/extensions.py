bf69edf1624d616ab15528df95d01c100547cf9e
Ironor25 <rock.on0103@gmail.com>
2026-01-29 23:03:19 +0530

    Init

--- app/extensions.py
+++ app/extensions.py
@@ -1,14 +1,11 @@
 import os
 from pymongo import MongoClient
 
-# Read Mongo URI from environment
 MONGO_URI = os.getenv("MONGO_URI")
 
-# Create Mongo client (created once, reused everywhere)
+#Mongo client (created once, reused everywhere)
 mongo_client = MongoClient(MONGO_URI)
 
-# Database
+# database
 db = mongo_client["github_events"]
-
-# Collection
 events_collection = db["events"]
